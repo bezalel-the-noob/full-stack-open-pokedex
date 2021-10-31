9c02f7f156675c93064500bf319c990cb89fba60
Pavel <imran.pavel@gmail.com>
2021-10-31 12:39:26 +0200

    refactoring..

--- cypress/integration/demo/demo.spec.js
+++ cypress/integration/demo/demo.spec.js
@@ -5,16 +5,14 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
-  
 
   it('pokemon page can be navigated to', function () {
     cy
       .get('.list-item')
       .contains('venusaur')
-      .click();
-    cy.wait(1000);
+      .click()
     cy
       .get('.pokemon-name')
-      .contains('venusaur');
-  });
+      .contains('venusaur')
+  })
 })
